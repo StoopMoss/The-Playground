90fda56cd604167c872bc4ff5483a08287c1816c
Conner <connermattingly@yahoo.com>
2018-09-15 23:03:48 -0700

    adding the Kata files

---
+++ Katas/FizzBuzz.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.Integer;
+
+public class FizzBuzz {
+
+	public static void main(String[] args){
+		for (int i = 0; i < 100; i++) {
+			System.out.println(Integer.toString(i));
+		}
+		this.print(); 
+	}
+
+	public void print(){
+		System.out.println("this is a test");
+		System.out.println("Can you Hear ME?");
+		System.out.println("Can you all See me?");
+	}
+}
